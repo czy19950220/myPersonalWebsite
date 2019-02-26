32d1cd75df4b374c00e8cca0e739243bb1920e19
czy19950220 <2772553223@qq.com>
2019-02-27 07:51:46 +0800

    按需引入element-ui

--- my-website/src/main.js
+++ my-website/src/main.js
@@ -5,16 +5,20 @@ import App from './App'
 import router from './router'
 import $ from "jquery";//引入jquery
 import {store} from './store/store' //引入store.js
-import VueFroala from 'vue-froala-wysiwyg';
-import FastClick from 'fastclick'
+import VueFroala from 'vue-froala-wysiwyg';//froala编辑器
+import FastClick from 'fastclick'//解决移动端点击延时
+import 'element-ui/lib/theme-chalk/index.css'//引入element-ui的css
+import element from './element/index'//按需引入element-ui
+
 
 Vue.config.productionTip = false
 
 require('es6-promise').polyfill();
 
+Vue.use(element);
 Vue.use(VueFroala);
 
-if ('addEventListener' in document) {
+if ('addEventListener' in document) {//fastclick的点击事件
   document.addEventListener('DOMContentLoaded', function() {
     FastClick.attach(document.body);
   }, false);
